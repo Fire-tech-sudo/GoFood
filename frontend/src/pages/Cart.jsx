be03ac347fa04463caf39a6ad4ff2f06ca954898
Fire-tech-sudo <ray968996@gmail.com>
2026-05-22 14:25:27 -0400

    Update Cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -9,7 +9,7 @@ export default function Cart() {
     const handleCheckOut = async () => {
         let userEmail = localStorage.getItem("userEmail");
 
-        let response = await fetch("http://localhost:3000/api/orderData", {
+        let response = await fetch("https://gofood-cz6f.onrender.com/api/orderData", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"

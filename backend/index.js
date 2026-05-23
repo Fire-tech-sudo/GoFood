a807d2196a38a2503181c90563b02ec4c567e99b
Fire-tech-sudo <ray968996@gmail.com>
2026-05-22 23:20:59 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const myOrderRoute = require('./routes/MyOrders');
 mongoDB();
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", " http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Origin", "https://go-food-cyan.vercel.app/");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
     next();
 })
